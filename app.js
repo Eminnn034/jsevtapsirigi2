3708a99f67181b8d02d7f15be8d9dc048bb0330e
Eminnn034 <emin.seyid97@gmail.com>
2024-10-28 19:02:54 +0400

    homework2

---
+++ app.js
@@ -0,0 +1,17 @@
+let fullName ="Emin Seyidov"
+console.log(fullName[0] , fullName [5]);
+
+
+const fullNamE = "Emin Seyidov"
+console.log(fullNamE.replace ("Emin Seyidov", "Seyidov Emin "));
+
+
+
+let text ="Emin Seyidov"
+console.log(text.length - 1 );
+
+
+let firstName = "Emin";
+let lastName = "Seyidov";
+console.log( "Sizin adiniz" + " " + firstName + " " + lastName + "." + "Sizi bir daha gormeyimize sadiq,")
+
